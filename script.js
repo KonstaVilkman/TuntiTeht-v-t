34644259aabb3218ef0f135622de33dcffe888e2
Konsta <konsta.vilkman@edu.salpaus.fi>
2020-11-16 11:44:22 +0000

    20201116

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 console.log("Täällä ollaan");
 
+function laskutehtava(ekaNumero, tokaNumero){
+  return (ekaNumero + tokaNumero);
+} 
+
 // muuttujat: let tai const
 
 let ika = 16;
@@ -106,3 +110,91 @@ const laksytTehtava = laksyt.map(function(laksy){
 });
 
   console.log(laksytTehtava);
+
+  console.log('------------');
+
+const tehtavaTehty = laksyt.filter(function(laksy){
+  return laksy.tehty ===false;
+});
+console.log(tehtavaTehty);
+
+const numero = 7;
+const nimi= 'jorma';
+
+if(numero < 18 || nimi === 'jorma'){
+  console.log('Olet alaikäinen tai Olet ystäväni Jorma')
+}else if(numero === 18){
+  console.log('Olet juuri täyttänyt 18!')
+}
+else{
+    console.log('Olet täysi-ikäinen')
+}
+const access = numero > 18 ? 'granted' : 'denied';
+console.log(access);
+
+function laskutehtava(){
+
+} 
+console.log(laskutehtava(5, 2));
+
+/////////////////////////////////////////////////////////////////////////////////
+const kurssinOsat = document.querySelectorAll('.sisalto');
+kurssinOsat.forEach((osa)=>console.log(osa));
+
+const ul = document.querySelector('.kurssi')
+
+const kurssinappi = document.querySelector('.kurssinappi')
+const nimiSisalto = document.querySelector('#nimi');
+const asiaSisalto = document.querySelector('#tieto');
+const virhe = document.querySelector('.virheilmoitus');
+const viesti = document.querySelector('#tiedot');
+
+kurssinappi.addEventListener('click', e=>{
+  e.preventDefault();
+  if(nimiSisalto.value === '' || asiaSisalto.value === ''){
+    virhe.classList.add('virhe');
+    virhe.innerHTML = 'Täytä kaikki kentät';
+    kurssinappi.style.background = 'red';
+    kurssinappi.value = 'Virhe, en lisännyt tietoa.';
+    setTimeout(()=> virhe.remove(), 3000);
+    setTimeout(()=> kurssinappi.style.background=rbg(245, 245, 245), 3000);
+    setTimeout(()=> kurssinappi.value = 'Lisää tieto', 3000);
+  }else{
+
+  const li = document.createElement('li');
+
+li.appendChild(document.createTextNode(`${nimiSisalto.value} : ${asiaSisalto.value}`));
+
+  viesti.appendChild(li);
+
+  nimiSisalto.value = '';
+  asiaSisalto.value = ''; 
+  }
+});
+
+const merkitseValmiit = document.querySelector('.merkinta');
+
+merkitseValmiit.addEventListener('click', e => {
+  e.preventDefault();
+  if(document.querySelector('#tehta').checked){
+    ul.children[0].innerHTML = '<input type="checkbox" id="tehta" checked>HTML tunnit pidetty';
+  }else{
+    ul.children[0].innerHTML = '<input type="checkbox" id="tehta">HTML';
+  }
+  if(document.querySelector('#tehtb').checked){
+    ul.children[1].innerHTML = '<input type="checkbox" id="tehtb" checked>CSS tunnit pidetty';
+  }else{
+    ul.children[1].innerHTML = '<input type="checkbox" id="tehtb">CSS';
+  }
+  if(document.querySelector('#tehtc').checked){
+    ul.children[2].innerHTML = '<input type="checkbox" id="tehtc" checked>JavaScript tunnit pidetty';
+  }else{
+    ul.children[2].innerHTML = '<input type="checkbox" id="tehtc">JavaScript';
+  }
+  if(document.querySelector('#tehtd').checked){
+    ul.children[3].innerHTML = '<input type="checkbox" id="tehtd" checked>CI/CD tunnit pidetty';
+  }else{
+    ul.children[3].innerHTML = '<input type="checkbox" id="tehtd">CI/CD';
+  }
+});
+
